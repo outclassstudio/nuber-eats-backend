5fa159b36fc946b048274fdce04fb9f85f4d00d3
outclassstudio <mlimshs@gmail.com>
2022-07-22 22:06:48 +0900

    Feat : Set mail service

---
+++ src/mail/mail.service.ts
@@ -0,0 +1,36 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { MailModuleOptions } from './mail.interface';
+import got from 'got';
+import * as FormData from 'form-data';
+
+@Injectable()
+export class MailService {
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
+  ) {
+    // this.sendEmail('test', 'testing...');
+    console.log('이건 어때', options);
+  }
+  private async sendEmail(subject: string, content: string) {
+    const form = new FormData();
+    form.append('from', `Excited User <mailgun@${this.options.domain}`);
+    form.append('to', `mlimshs@naver.com`);
+    form.append('subject', subject);
+    form.append('text', content);
+
+    const response = await got(
+      `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `api:${this.options.apiKey}`,
+          ).toString('base64')}`,
+        },
+        body: form,
+      },
+    );
+    console.log(response);
+  }
+}
